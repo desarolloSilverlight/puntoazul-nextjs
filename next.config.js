5653e7c150e6cabfe5daaa8d532f33508de1519d
JCamiloC <cerinzajuanc@gmail.com>
2025-02-11 17:26:34 -0500

    Ajuste Redireccion

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   output: "standalone", // Agrega esto si falta
 };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/auth/login',
+        permanent: true, // true = redirección permanente (301)
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;

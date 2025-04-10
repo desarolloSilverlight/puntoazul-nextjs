3c2ea8b7fe868c4f91c7e86d8454b96228bc7523
Juan Chaparro <cerinzajuanc@gmail.com>
2025-04-10 11:13:57 -0500

    Adds dynamic HeaderStats rendering based on user role
    Introduces localStorage integration to retrieve the user role.
    Implements conditional rendering of HeaderStats components tailored to
    specific user roles (Administrator, Employee, Associate, Linked).
    Improves UI flexibility and personalization for different user types.

--- layouts/Admin.js
+++ layouts/Admin.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // components
-
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
 import HeaderStats from "components/Headers/HeaderStats.js";
+import HeaderStatsB from "components/Headers/HeaderStatsB.js";
+import HeaderStatsF from "components/Headers/HeaderStatsF.js";
 import FooterAdmin from "components/Footers/FooterAdmin.js";
 
 export default function Admin({ children, username }) {
+  const [perfil, setPerfil] = useState("");
+
+  // Obtener el perfil desde localStorage
+  useEffect(() => {
+    const storedPerfil = localStorage.getItem("perfil");
+    setPerfil(storedPerfil);
+  }, []);
+
+  // Renderizar el componente HeaderStats según el perfil
+  const renderHeaderStats = () => {
+    if (perfil === "Administrador" || perfil === "Empleado") {
+      return <HeaderStats />;
+    } else if (perfil === "Asociado") {
+      return <HeaderStatsB />;
+    } else if (perfil === "Vinculado") {
+      return <HeaderStatsF />;
+    } else {
+      return null; // Si no hay perfil válido, no renderizar nada
+    }
+  };
+
   return (
     <>
       <Sidebar />
       <div className="relative md:ml-64 bg-blueGray-100">
-        <AdminNavbar username={username}/>
+        <AdminNavbar username={username} />
         {/* Header */}
-        <HeaderStats />
+        {renderHeaderStats()}
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           {children}
           <FooterAdmin />

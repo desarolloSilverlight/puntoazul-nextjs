fc293ca65e1c810d20a679bb462d50761d0a1f79
Juan Chaparro <cerinzajuanc@gmail.com>
2025-05-29 20:39:58 -0500

    Replaces static card stats with dynamic image carousel
    Introduces a responsive image carousel using 'react-slick' for improved visual presentation. Removes hardcoded card stats in favor of a flexible overlay design atop the carousel. Enhances user experience with autoplay, fade transitions, and customizable carousel height.

--- components/Headers/HeaderStatsF.js
+++ components/Headers/HeaderStatsF.js
@@ -1,70 +1,65 @@
 import React from "react";
-
-// components
-
-import CardStats from "components/Cards/CardStats.js";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
 
 export default function HeaderStats() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    fade: true,
+    cssEase: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+  };
+
+  const images = [
+    "/img/carruselF/img1.PNG",
+    "/img/carruselF/img2.png",
+    "/img/carruselF/img3.png",
+    "/img/carruselF/img4.png"
+  ];
+
+  const carouselHeight = '390px'; // Puedes ajustar esto
+
   return (
-    <>
-      {/* Header */}
-      <div className="relative bg-green md:pt-32 pb-32 pt-12">
-        <div className="px-4 md:px-10 mx-auto w-full">
-          <div>
-            {/* Card stats */}
-            <div className="flex flex-wrap">
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
-                <CardStats
-                  statSubtitle="Total empaque primario"
-                  statTitle="350,897"
-                  statArrow="up"
-                  statPercent="3.48"
-                  statPercentColor="text-emerald-500"
-                  statDescripiron="Since last month"
-                  statIconName="far fa-chart-bar"
-                  statIconColor="bg-red-500"
-                />
-              </div>
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
-                <CardStats
-                  statSubtitle="NEW USERS"
-                  statTitle="2,356"
-                  statArrow="down"
-                  statPercent="3.48"
-                  statPercentColor="text-red-500"
-                  statDescripiron="Since last week"
-                  statIconName="fas fa-chart-pie"
-                  statIconColor="bg-orange-500"
-                />
-              </div>
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
-                <CardStats
-                  statSubtitle="SALES"
-                  statTitle="924"
-                  statArrow="down"
-                  statPercent="1.10"
-                  statPercentColor="text-orange-500"
-                  statDescripiron="Since yesterday"
-                  statIconName="fas fa-users"
-                  statIconColor="bg-pink-500"
-                />
-              </div>
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
-                <CardStats
-                  statSubtitle="PERFORMANCE"
-                  statTitle="49,65%"
-                  statArrow="up"
-                  statPercent="12"
-                  statPercentColor="text-emerald-500"
-                  statDescripiron="Since last month"
-                  statIconName="fas fa-percent"
-                  statIconColor="bg-lightBlue-500"
-                />
-              </div>
-            </div>
+    <div className="relative w-full min-h-[390px] overflow-hidden">
+      {/* Carrusel principal */}
+      <Slider {...settings} className="w-full h-full">
+        {images.map((img, index) => (
+          <div key={index} className="w-full h-full">
+            <Image
+              src={img}
+              alt={`Background ${index + 1}`}
+              className="w-full h-full object-cover object-center"
+              loading="lazy"
+              width={1920} // mejor resolución si es para pantalla completa
+              height={390}
+            />
           </div>
+        ))}
+      </Slider>
+
+      {/* Overlay para contraste */}
+      <div className="absolute inset-0 bg-black/30 z-0" />
+
+      {/* Contenido encima del carrusel */}
+      <div className="absolute inset-0 z-10 flex flex-col justify-between p-8 text-white">
+        <div className="flex justify-between items-start">
+          <div className="space-x-6">
+            <a href="#" className="hover:underline">Information</a>
+            <a href="#" className="hover:underline">Productos</a>
+          </div>
+          <span className="font-medium">ADMINISTRATOR</span>
         </div>
+
+        <div>{/* Aquí puedes meter tus tarjetas si las usas en el futuro */}</div>
       </div>
-    </>
+    </div>
   );
 }
